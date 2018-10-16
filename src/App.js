724755bca1d1303a14f4d3d35f5ec05dc67f7eb5
Yvan Pangilinan <yvan@organicstart.com>
2018-10-16 13:05:41 -0400

    added BatchOrder Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
-
+import BatchOrder from './components/BatchOrder/BatchOrder';
+import 'tachyons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App tc">
         <header>
           <img src={logo} style={{textAlign:'center', width:'250px', height:'auto'}} alt="logo" />
         </header>
-        <div>
-          <form>
-            <label> 
-            Batch Number:
-            <input type="text" />
-            </label>
-            <label> 
-            Picked By:
-            <input type="text" />
-            </label>
-            <label> 
-            Shipped By:
-            <input type="text" />
-            </label>
-            <input type="submit" value="Generate Batch" />
-          </form>
-        </div>
+        <BatchOrder />
         <div>
           <form>
            <label> 

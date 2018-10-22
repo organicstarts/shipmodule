9ea684304189cd05bb58953dd73c23ebf2fbfb38
Yvan Pangilinan <yvan@organicstart.com>
2018-10-22 17:58:21 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

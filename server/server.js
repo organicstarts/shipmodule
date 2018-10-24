d73370a7438255e781bdad6bbddbb79fe541cc05
Yvan Pangilinan <yvan@organicstart.com>
2018-10-24 12:14:10 -0400

    fixed file structure + setup express

--- server/server.js
+++ server/server.js
@@ -1,23 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "../build")));
+import bodyParser from 'body-parser'
+import express from 'express'
+import path from 'path'
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
+const router = express.Router()
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
 
-app.get('/ping', (req, res) => {
-  console.log('pong');
+router.get('/cities', (req, res) => {
+  const cities = [
+    {name: 'New York City', population: 8175133},
+    {name: 'Los Angeles',   population: 3792621},
+    {name: 'Chicago',       population: 2695598}
+  ]
+  res.json(cities)
 })
 
 
-app.listen(process.env.PORT || 3001 ,() => {
-  console.log("server is running");
-  
-});
+app.use(router)
+
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles)
+
+app.set('port', (process.env.PORT || 3001))
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`)
+})

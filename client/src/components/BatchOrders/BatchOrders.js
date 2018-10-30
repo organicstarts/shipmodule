bf5036e92eec0f3b3964762a4ad35b8ffea08554
Isaiah Marc Sanchez <isaiah.marc.sanchez@gmail.com>
2018-10-30 17:15:32 -0400

    Fixed CORS.

--- client/src/components/BatchOrders/BatchOrders.js
+++ client/src/components/BatchOrders/BatchOrders.js
@@ -1,66 +1,46 @@
 import React from 'react';
 import { Segment, Button, Form } from 'semantic-ui-react';
-import axios from 'axios';
 import people from '../../config/people';
-import shipstation from '../../config/auth';
+import * as config from '../../config/auth';
 
 class BatchOrders extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {batchNumber: '', picker: '', shipper: ''};
+        this.state = { batchNumber: '', picker: '', shipper: '' };
         this.handleChange = this.handleChange.bind(this);
         this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange = (e) => this.setState({[e.target.name]: e.target.value});
+    handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-    handleSelectChange = (e, data) => this.setState({[data.name]: data.value});
+    handleSelectChange = (e, data) => this.setState({ [data.name]: data.value });
 
     handleSubmit(event) {
-        var config = {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            },
-            params: {
-                  email_address: shipstation.user,
-                  password: shipstation.key
-            }
-     };
-     axios.get( 'https://ssapi.shipstation.com/shipments', {}, config)
-     .then(function (response) {
-            console.log(response);
-     })
-     .catch(function (response) {
-            console.log(response);
-     });
-        
-        
-        
-        /*
-        axios.get('https://ssapi.shipstation.com/shipments', {
-            params: {
-                ID: shipstation.user,
-                password: shipstation.key
-            }
-        }).then(response => console.log(response))
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://ssapi.shipstation.com/shipments');
-        const encodedString = new Buffer(shipstation.user + ':' + shipstation.key).toString('base64');
-        request.setRequestHeader('Authorization', 'Basic ' + encodedString);
 
-        request.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-        }
+        const encodedString = new Buffer(`${config.shipstation.user}:${config.shipstation.key}`).toString('base64');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 
+                "Access-Control-Allow-Origin": "*", 
+                "Authorization": `Basic ${encodedString}` 
+            }
         };
-*/
-        //request.send();
 
-      //  console.log(request);
+        return fetch('https://ssapi.shipstation.com/shipments', requestOptions)
+            .then(function (response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(function (responseAsJson) {
+                console.log(responseAsJson);
+            })
+            .catch(function (error) {
+                console.log('Looks like there was a problem: \n', error);
+            });
     }
     render() {
         return (
@@ -70,8 +50,8 @@ class BatchOrders extends React.Component {
                         <Form.Input fluid label='Batch Number' placeholder='123456' name='batchNumber' value={this.state.batchNumber} onChange={this.handleChange} />
                     </Form.Field>
                     <Form.Group widths='equal'>
-                            <Form.Select label='Picker' placeholder='Select One' name='picker' options={people} onChange={this.handleSelectChange} />
-                            <Form.Select label='Shipper' placeholder='Select One' name='shipper' options={people} onChange={this.handleSelectChange} />
+                        <Form.Select label='Picker' placeholder='Select One' name='picker' options={people} onChange={this.handleSelectChange} />
+                        <Form.Select label='Shipper' placeholder='Select One' name='shipper' options={people} onChange={this.handleSelectChange} />
                     </Form.Group>
                     <Button size='large' color='olive' type='submit'>Generate Batch</Button>
                 </Form>

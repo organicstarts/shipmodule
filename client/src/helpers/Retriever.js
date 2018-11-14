647a2f9effc6598c470732972f23466e0cb31277
Yvan Pangilinan <yvan@organicstart.com>
2018-11-14 15:41:53 -0500

    moved proxy for bigcommerce

--- client/src/helpers/Retriever.js
+++ client/src/helpers/Retriever.js
@@ -12,13 +12,14 @@ import * as config from "../config/auth";
 import * as log from "./Log";
 
 function switchURL(destination) {
+  let proxy = "https://cors-anywhere.herokuapp.com/";
   switch (destination) {
     default:
       return log.error("Invalid API definition supplied to switchURL.");
     case "ss":
       return "https://ssapi.shipstation.com/";
     case "os":
-      return "https://organicstart.com/api/v2/";
+      return proxy + "https://organicstart.com/api/v2/";
     case "osw":
       return "https://organic-start-wholesale.myshopify.com/";
     case "tfc":
@@ -57,7 +58,6 @@ function authHeader(destination) {
 }
 
 export const fetchJSON = async (destination, path) => {
-  let proxy="https://cors-anywhere.herokuapp.com/"
   return await axios
     .get(switchURL(destination) + path, authHeader(destination))
     .catch(log.error);

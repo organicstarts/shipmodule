27e700769d02f993f40e735451cf242a5e86edf4
Yvan Pangilinan <yvan@organicstart.com>
2018-11-29 13:41:48 -0500

    fix database issues

--- client/src/config/firebaseconf.js
+++ client/src/config/firebaseconf.js
@@ -1,21 +1,22 @@
 import firebase from "firebase/app";
-import 'firebase/app';
-import 'firebase/auth'
+import "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
-const  config = {
-    apiKey: "AIzaSyDUw_5o7m_2HJfF7V5_gaJhvAXAhnrc6uc",
-    authDomain: "shipmodule.firebaseapp.com",
-    databaseURL: "https://shipmodule.firebaseio.com",
-    projectId: "shipmodule",
-    storageBucket: "shipmodule.appspot.com",
-    messagingSenderId: "802645309579"
-  };
+const config = {
+  apiKey: "AIzaSyDUw_5o7m_2HJfF7V5_gaJhvAXAhnrc6uc",
+  authDomain: "shipmodule.firebaseapp.com",
+  databaseURL: "https://shipmodule.firebaseio.com",
+  projectId: "shipmodule",
+  storageBucket: "shipmodule.appspot.com",
+  messagingSenderId: "802645309579"
+};
 
-  firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: 'select_account'
-})
+  prompt: "select_account"
+});
 export const auth = firebase.auth();
 export default firebase;

aa03ed15d397ca43cefa9dc8c3e38e96a823dc99
Yvan Pangilinan <yvan@organicstart.com>
2018-12-12 12:19:39 -0500

    tk kit fix

--- client/src/components/BatchOrders/BatchOrders.js
+++ client/src/components/BatchOrders/BatchOrders.js
@@ -172,6 +172,7 @@ Special case sku.includes("TK || first char is an integer") => parse data first 
       }
       for (let sku of tkSku) {
         if (sku.sku.includes(key)) {
+          sku.isTk = false
           if (group[key][0].combineTotal) {
             group[key][0].combineTotal += sku.quantity;
           } else {

0dd5b4cd03ae0db4460fa6c1d109958566767fed
Yvan Pangilinan <yvan@organicstart.com>
2019-05-03 17:47:45 -0400

    added jason and pete to peoplejs

--- client/src/config/peopleicon.js
+++ client/src/config/peopleicon.js
@@ -8,96 +8,93 @@ import {
   training,
   jonathan,
   steve,
-  chris,
-  jason, 
+  bigjason,
+  skinnyjason,
   peter,
   isaiah,
   yvan,
   chandler
 } from "../images/employeeIcons";
 
-
-
 export const iconQuotes = {
-    david: {
-        name: "David",
-        quote: "Life Lesson. Never assume anything!",
-        icon: david
-    },
-    julian: {
-        name: "Julian",
-        quote: "Embrace the struggle",
-        icon: julian
-    },
-    luis: {
-        name: "Luis",
-        quote: "Paz y Amor",
-        icon: luis
-    },
-    richard: {
-        name: "Richard",
-        quote: "It matters not how long we live but how.",
-        icon: richard
-    },
-    romario: {
-        name: "Romario",
-        quote: "Working Hard or Hardly Working",
-        icon: romario
-    },
-    training: {
-        name: "Training",
-        quote: "Sorry! I'm learning. :)",
-        icon: training
-    },
-    jonathan: {
-        name: "Jonathan",
-        quote: "Still waters run deep.",
-        icon: jonathan
-    },
-    steve: {
-        name: "Steve",
-        quote: "There is good in everyday.",
-        icon: steve
-    },
-    chris: {
-        name: "Chris",
-        quote: "May the force be with you.",
-        icon: chris
-    },
-    jason: {
-        name: "Jason",
-        quote: "Here for a good time, not a long time.",
-        icon: jason
-    },
-    john: {
-        name: "John",
-        quote: "With great power, comes great responsibilities.",
-        icon: daniel
-    },
-    chandler: {
-        name: "Chandler",
-        quote: "The world is not enough!",
-        icon: chandler
-    },
-    joseph: {
-        name: "Joseph",
-        quote: "With great power, comes great responsibilities.",
-        icon: daniel
-    },
-    yvan: {
-        name: "yvan",
-        quote: "Pow right in the kisser",
-        icon: yvan
-    },
-    peter: {
-        name: "peter",
-        quote: "boss",
-        icon: peter
-    }
-    ,
-    isaiah: {
-        name: "isaiah",
-        quote: "...",
-        icon: isaiah
-    }
-}
+  david: {
+    name: "David",
+    quote: "Life Lesson. Never assume anything!",
+    icon: david
+  },
+  julian: {
+    name: "Julian",
+    quote: "Embrace the struggle",
+    icon: julian
+  },
+  luis: {
+    name: "Luis",
+    quote: "Paz y Amor",
+    icon: luis
+  },
+  richard: {
+    name: "Richard",
+    quote: "It matters not how long we live but how.",
+    icon: richard
+  },
+  romario: {
+    name: "Romario",
+    quote: "Working Hard or Hardly Working",
+    icon: romario
+  },
+  training: {
+    name: "Training",
+    quote: "Sorry! I'm learning. :)",
+    icon: training
+  },
+  jonathan: {
+    name: "Jonathan",
+    quote: "Still waters run deep.",
+    icon: jonathan
+  },
+  steve: {
+    name: "Steve",
+    quote: "There is good in everyday.",
+    icon: steve
+  },
+  bigjason: {
+    name: "Jason",
+    quote: "FOR THE YOUTH!",
+    icon: bigjason
+  },
+  skinnyjason: {
+    name: "Jason",
+    quote: "Here for a good time, not a long time.",
+    icon: skinnyjason
+  },
+  john: {
+    name: "John",
+    quote: "With great power, comes great responsibilities.",
+    icon: daniel
+  },
+  chandler: {
+    name: "Chandler",
+    quote: "The world is not enough!",
+    icon: chandler
+  },
+  joseph: {
+    name: "Joseph",
+    quote: "With great power, comes great responsibilities.",
+    icon: daniel
+  },
+  yvan: {
+    name: "yvan",
+    quote: "Pow right in the kisser",
+    icon: yvan
+  },
+  peter: {
+    name: "peter",
+    quote: "That's right I still work!",
+    icon: peter
+  },
+  isaiah: {
+    name: "isaiah",
+    quote: "...",
+    icon: isaiah
+  }
+};

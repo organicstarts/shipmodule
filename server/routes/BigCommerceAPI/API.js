ab658e891b44706bef7a6e0512fcad59e8755254
Yvan Pangilinan <yvan@organicstart.com>
2019-06-18 16:21:07 -0400

    added deductinventory api

--- server/routes/BigCommerceAPI/API.js
+++ server/routes/BigCommerceAPI/API.js
@@ -2,7 +2,7 @@ import "module-alias/register";
 const router = require("express").Router();
 const fetch = require("node-fetch");
 import bigCommerce from "@bgauth/auth.json";
-// import productInfo from "@bgauth/productInfo";
+import pInfo from "@bgauth/productInfo";
 const moment = require("moment");
 const username = bigCommerce.bigcommerce.user;
 const password = bigCommerce.bigcommerce.key;
@@ -197,31 +197,31 @@ router.get("/getinventorylevel", (req, res) => {
 /*-------------------------------------------------------------------
                             POST REQUESTS                            
 ---------------------------------------------------------------------*/
-// router.post("/deductbundletosingle", (req, res) => {
-//   //build api URL with user ordernumber to see if order had coupons used
-//   const baseUrl = `http://localhost:3001/os/updateinventory`;
-//   res.set({
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, HEAD"
-//   });
-//   Promise.all(
-//     req.body.line_items.map(async data => {
-//       await fetch(baseUrl, {
-//         method: "PUT",
-//         headers: {
-//           "Access-Control-Allow-Origin": "*",
-//           Authorization: `Basic ${encodedString}`,
-//           "Content-Type": "application/json",
-//           Accept: "application/json"
-//         },
-//         body: JSON.stringify({
-//           inventory_level: 0 - data.quantity,
-//           productID: productInfo[data.sku].productID
-//         })
-//       });
-//     })
-//   );
-// });
+router.post("/deductbundletosingle", (req, res) => {
+  //build api URL with user ordernumber to see if order had coupons used
+  const baseUrl = `https://brainiac.organicstart.com/os/updateinventory`;
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, HEAD"
+  });
+  Promise.all(
+    req.body.line_items.map(async data => {
+      await fetch(baseUrl, {
+        method: "PUT",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Basic ${encodedString}`,
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          inventory_level: 0 - data.quantity,
+          productID: pInfo[data.sku].productID
+        })
+      });
+    })
+  ).then(x => res.json({ msg: "success" })).catch(x => res.json({msg: "fail"}))
+});
 
 /*-------------------------------------------------------------------
                             PUT REQUESTS                            
@@ -235,7 +235,7 @@ router.put("/updateinventory", (req, res) => {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, HEAD"
   });
-  console.log(req.body);
+
   if (req.body.noEquation) {
     let total = parseInt(req.body.inventory_level);
     fetch(baseUrl, {
